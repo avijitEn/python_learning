e5e806a153b860e5608a0fb7ddd97088f9d31700
avijitEn <jitt.198@gmail.com>
2019-12-26 17:44:04 +0530

    remove the empty space in file

---
+++ File Handling.py
@@ -0,0 +1,39 @@
+
+file=open("myfile.txt","w")
+file.write("""ttttttttttttttt
+2nd line 
+34d 
+
+last line
+""")
+file.close()
+data=open("myfile.txt","r")
+data.close()
+#print(str(data.read().split()))
+#for each in data:
+    #print (each.strip().strip())
+#print ("".join([s for s in data.strip().splitlines(True) if s.strip("\r\n").strip()]))
+
+with open("myfile.txt","r") as data:
+    f=data.read()
+    #print(f.strip())
+
+#now remove the space line
+data=open("myfile.txt","r")
+
+list=[]
+counter=0
+for line in data:
+    counter += 1
+    if not line.isspace():
+        list.append(line)
+        temp=line.strip()
+        print(temp,counter)
+    #print(f"{counter}  {line}")
+
+
+
+
+
+
+
